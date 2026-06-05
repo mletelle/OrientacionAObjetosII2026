5e3bc122de25d22a1eadb7f8e8ee51b0453dd83b
mletelle <mauro.letelle@gmail.com>
2026-06-05 19:09:55 -0300

    mini update constantes

--- tpobserver/src/main/java/unrn/clima/ObservadorConsola.java
+++ tpobserver/src/main/java/unrn/clima/ObservadorConsola.java
@@ -1,14 +1,17 @@
 package unrn.clima;
 
 public class ObservadorConsola implements Observador {
+    private static final int UMBRAL_CALOR = 17;
+    private static final int UMBRAL_FRIO = 12;
+
     @Override
     public void actualizar(String temperatura) {
         double valor = convertirANumero(temperatura);
-        if (valor < 12) {
+        if (valor < UMBRAL_FRIO) {
             System.out.println("Hace frio, se encenderá la caldera");
         }
 
-        if (valor > 17) {
+        if (valor > UMBRAL_CALOR) {
             System.out.println("Hace calor, se encenderá el aire acondicionado");
         }
     }
